a9134de6727ee317e59e6006846396d03bbe4023
Martin Mullins <martin.mullins942+git@gmail.com>
2020-11-13 11:48:22 +0000

    improve build script, GameMP, EntitiesMP and ShadersMP all build

--- Sources/build-em.sh
+++ Sources/build-em.sh
@@ -10,27 +10,37 @@ SRC_ROOT="$GIT_ROOT/Sources"
 set -e
 set -x
 
-rm -rf cmake-em-build
-mkdir cmake-em-build
-cd cmake-em-build
+cm() {
+  rm -rf cmake-em-build
+  mkdir cmake-em-build
+  pushd cmake-em-build
 
-#cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 ..
-#ninja
+  emcmake cmake \
+    -DECC="$GIT_ROOT/ecc" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_FLAGS=-m32 \
+    -DCMAKE_CXX_FLAGS=-m32 \
+    -DUSE_I386_NASM_ASM=FALSE \
+    "$SRC_ROOT"
 
-emcmake cmake \
-  -DECC="$GIT_ROOT/ecc" \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_FLAGS=-m32 \
-  -DCMAKE_CXX_FLAGS=-m32 \
-  -DUSE_I386_NASM_ASM=FALSE \
-  "$SRC_ROOT"
 
-echo "Then the rest..."
-emmake make ParseEntities
-emmake make VERBOSE=1 EntitiesMP
+   popd
+}
 
+mk() {
+  echo "Then the rest..."
+  emmake make -C cmake-em-build ParseEntities
+  emmake make -C cmake-em-build EntitiesMP
+  emmake make -C cmake-em-build GameMP
+  emmake make -C cmake-em-build Shaders
+}
+
+mktarg() {
+  emmake make -C cmake-em-build VERBOSE=1 "$1"
+}
+
+$*
 #mv Debug Bin
 #mv ssam* Bin/
 #cp -vr "$SRC_ROOT"/../template/* ./
 
-cd -
